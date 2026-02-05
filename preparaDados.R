671953d6ca83b12c8f824bceafafe37982bc4d2c
Isaac Medeiros <127994907+MedeirosIsaac@users.noreply.github.com>
2026-02-05 11:27:36 -0300

    Update preparaDados.R

--- preparaDados.R
+++ preparaDados.R
@@ -2,24 +2,22 @@ library(vroom)
 library(dplyr)
 library(tibble)
 
-setwd("~/AppShiny/") # Ajuste para o seu caminho
+setwd("~/BrainMap/") 
 
-# 1. Carregar CSVs
 rows <- vroom("genes_matrix_csv/rows_metadata.csv")
 columns <- vroom("genes_matrix_csv/columns_metadata.csv")
 
-# 2. Montar a Matriz de Expressão (Counts)
 counts_raw <- vroom(
   "genes_matrix_csv/expression_matrix.csv",
   col_names = FALSE
 ) %>%
   dplyr::select(-1)
 
-# Converter para Matriz Numérica (Muito mais leve que DataFrame)
+#Matrix numerica
 matriz_expressao <- as.matrix(counts_raw)
 
 colnames(matriz_expressao) <- columns$column_num
-rownames(matriz_expressao) <- rows$gene_symbol # Assumindo que a ordem bate
+rownames(matriz_expressao) <- rows$gene_symbol
 
 # 3. Pré-calcular Log2 (Para não fazer isso a cada clique)
 # Lidando com duplicatas de genes (somando ou tirando média) se houver,
@@ -28,12 +26,10 @@ genes_unicos <- !duplicated(rownames(matriz_expressao))
 matriz_expressao <- matriz_expressao[genes_unicos, ]
 matriz_final <- log2(matriz_expressao + 1)
 
-# 4. Salvar tudo em um único arquivo RDS
-# Salvamos também os metadados necessários
 dados_app <- list(
   expression_matrix = matriz_final,
   col_meta = columns,
-  gene_list = rownames(matriz_final) # Lista pronta para o UI
+  gene_list = rownames(matriz_final)
 )
 
 saveRDS(dados_app, "dados_otimizados.rds")
